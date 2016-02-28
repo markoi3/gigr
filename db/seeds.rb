c8eb8f1bef9b839362ebeaa7f3f7f7669ee06dac
Marko Islamovic <marko@gigrove.com>
2016-02-28 16:49:55 +0000

    Added database seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: "markoi1", email: "marko1@gigrove.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "markoi2", email: "marko2@gigrove.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "markoi3", email: "marko3@gigrove.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "markoi4", email: "marko4@gigrove.com", password: "lovestuk", password_confirmation: "lovestuk")
+User.create(username: "markoi5", email: "marko5@gigrove.com", password: "lovestuk", password_confirmation: "lovestuk")
+p "Test users created"
